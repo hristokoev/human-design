6a6133c9ce3b49ec3342cf2a80c657b03ce4ea82
Hristo Koev <iamhristokoev@gmail.com>
2024-01-17 10:18:43 +0100

    Add hover to button

--- src/components/cards/Card02.tsx
+++ src/components/cards/Card02.tsx
@@ -30,9 +30,9 @@ export default function Card02({ image, title, text, cta, button, index }: Card0
 					{cta && (
 						<a className="btn border-2 border-black hover:bg-black/10 rounded-3xl" href="#">{cta}</a>
 					)}
-					<a href="#" className="flex items-center gap-2">
+					<a href="#" className="flex items-center gap-2 hover:text-[#FAC61B] group">
 						{button}
-						<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg" className="group-hover:rotate-90 transition-all duration-150 ease-in-out">
 							<path fillRule="evenodd" clipRule="evenodd" d="M12.7979 10.0992C12.9787 10.28 12.9787 10.5731 12.7979 10.754L8.07708 15.4747C7.89625 15.6556 7.60312 15.6556 7.42229 15.4747L7.20398 15.2565C7.02315 15.0756 7.02315 14.7825 7.20398 14.6016L11.3791 10.4266L7.20398 6.25149C7.02315 6.07066 7.02315 5.77753 7.20398 5.5967L7.42229 5.37839C7.60312 5.19756 7.89625 5.19756 8.07708 5.37839L12.7979 10.0992Z" fill="black" />
 						</svg>
 					</a>
