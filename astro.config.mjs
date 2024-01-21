71972eab7fb06d7e315c5743ccbc15f584b292e4
Hristo Koev <iamhristokoev@gmail.com>
2024-01-21 22:03:19 +0100

    serverless adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercelServerless from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
@@ -7,8 +7,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react(), tailwind()],
-	output: 'server',
-  adapter: vercel(),
+  output: 'server',
+  adapter: vercelServerless(),
 	redirects: {
 		"/blog": "/blog/1",
 	}
