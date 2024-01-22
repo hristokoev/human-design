de185eab7b99fde2a22ab4bda519adbbbc254960
Hristo Koev <iamhristokoev@gmail.com>
2024-01-22 14:20:02 +0100

    back to hybrid, SSR for mam-zajem

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react(), tailwind()],
-	output: 'server',
+	output: 'hybrid',
   adapter: vercel(),
 	redirects: {
 		"/blog": "/blog/1",
